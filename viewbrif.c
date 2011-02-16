ac000/viewbrif

commit 44ab3a6a1da95dcc1c3a0925d715607dfeaceb79

Display the name of the file in the window title bar
When a brif file is loaded, display the name of the file in the
window title bar, e.g

	ViewBRIF (/tmp/brifn527.req)

viewbrif.c
@@ -32,7 +32,7 @@
 #include "brif_spec.h"
 
 /* Update for application version. */
-#define VERSION		"020"
+#define VERSION		"021"
 
 /*
  * DEBUG levels
@@ -95,6 +95,19 @@ static void reset_stats()
 	brif_stats.credits_amt = 0;
 	brif_stats.vat_ta      = 0;
 	brif_stats.file_size   = 0;
+}
+
+/*
+ * Set the window title to 'ViewBRIF (/path/to/brifile)'
+ */
+static void set_window_title(GtkWidget *window, char *extra_title)
+{
+	char *window_title;
+
+	window_title = malloc(12 + strlen(extra_title) * sizeof(char *));
+	sprintf(window_title, "ViewBRIF (%s)", extra_title);
+	gtk_window_set_title(GTK_WINDOW(window), window_title);
+	free(window_title);
 }
 
 /*
@@ -288,10 +301,10 @@ static void cb_about_window()
 	gtk_widget_show(about);
 }
 
-static void cb_file_chooser()
+static void cb_file_chooser(GtkWidget *widget, gpointer data)
 {
 	GtkWidget *file_chooser;
-	char *filename;
+	char *filename = NULL;
 
 	/* Create a new file selection widget */
 	file_chooser = gtk_file_chooser_dialog_new("File selection",
@@ -312,6 +325,7 @@ static void cb_file_chooser()
 	}
 
 	gtk_widget_destroy(file_chooser);
+	set_window_title(data, filename);
 }
 
 static void cb_quit(GtkMenuItem *menuitem, gpointer user_data)
@@ -951,7 +965,7 @@ int main(int argc, char *argv[])
 	g_signal_connect((gpointer)filemenu_new_instance, "activate",
 					G_CALLBACK(cb_new_instance), NULL);
 	g_signal_connect((gpointer)filemenu_open, "activate",
-					G_CALLBACK(cb_file_chooser), NULL);
+					G_CALLBACK(cb_file_chooser), window);
 	g_signal_connect((gpointer)helpmenu_about, "activate",
 					G_CALLBACK(cb_about_window), NULL);
 
@@ -982,9 +996,11 @@ int main(int argc, char *argv[])
 	gtk_widget_show(window);
 
 	/* If we got a filename as an argument, open that up in the viewer. */
-	if (argc > 1)
+	if (argc > 1) {
 		g_thread_create((GThreadFunc)read_file_thread, argv[1],
 								FALSE, NULL);
+		set_window_title(window, argv[1]);
+	}
 
 	gtk_main();
 	gdk_threads_leave();

viewbrif.h
@@ -10,6 +10,7 @@
  */
 
 static void reset_stats();
+static void set_window_title(GtkWidget *window, char *extra_title);
 static double add_dp(long int amount);
 static char *str_pad(char *newstr, char *str, int len, char *padchar, int just);
 static void create_tags(GtkTextBuffer *buffer);
@@ -19,7 +20,7 @@ static gboolean cb_search(GtkWidget *search_button, gpointer data);
 static void cb_about_window();
 static void cb_quit(GtkMenuItem *menuitem, gpointer user_data);
 static void cb_new_instance();
-static void cb_file_chooser();
+static void cb_file_chooser(GtkWidget *widget, gpointer data);
 static void process_line(char *fline, const int line_array[][2], 
 						const char *field_headers[]);
 static void display_raw_line(char *fline, const int line_array[][2]);

commit 87bd8af63ebdef6d1c5dfd3f75b463db5b5515cd

Fix a segfault introduced in 44ab3a6a
In 44ab3a6a we set the window title to include the name of the
currently viewed brif file. However if we opened the file
selection and just cancelled it, we'd end up trying to set the
window title to null.

Only set the window title if it's not null.

viewbrif.c
@@ -32,7 +32,7 @@
 #include "brif_spec.h"
 
 /* Update for application version. */
-#define VERSION		"023"
+#define VERSION		"023.90"
 
 /*
  * DEBUG levels
@@ -328,7 +328,8 @@ static void cb_file_chooser(GtkWidget *widget, gpointer data)
 	}
 
 	gtk_widget_destroy(file_chooser);
-	set_window_title(data, filename);
+	if (filename)
+		set_window_title(data, filename);
 }
 
 static void cb_quit(GtkMenuItem *menuitem, gpointer user_data)