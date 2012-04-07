bce5b4813e0b009f4d545c6f3f569bfee2ca1618
Andrew Clayton <andrew@digital-domain.net>
2012-04-07 16:49:51 +0100

    viewbrif: Update copyright years in the about window
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- viewbrif.c
+++ viewbrif.c
@@ -292,7 +292,7 @@ static void cb_about_window(void)
 	gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(about), "ViewBRIF");
 	gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(about), VERSION);
 	gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(about),
-				"Copyright (C) 2006-2011 Andrew Clayton");
+				"Copyright (C) 2006-2012 Andrew Clayton");
 	gtk_about_dialog_set_authors(GTK_ABOUT_DIALOG(about),
 						(const gchar **)&authors);
 
